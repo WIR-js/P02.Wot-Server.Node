7884926cbdfc2d35eac89b20888d29a22af73f0d
farzadflp <36715834+farzadflp@users.noreply.github.com>
2018-09-21 19:22:02 +0430

    edit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-
 var httpServer = require('./server/http');
-
 var server = httpServer.listen(8000);
+var dhtPlugin = require('./plugins/DHT11');
+dhtPlugin = require('./plugins/DHT22SensorPlugin');
 
+dhtPlugin.start({'simulate': true, 'frequency': 10000});
 console.log("http//127.0.0.1:8000");
+
+
