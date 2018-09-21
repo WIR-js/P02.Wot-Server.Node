abe5d4b79018fc92bf9b4fee9efb6f0243185e9d
Pi <h.larian@yahoo.com>
2018-09-22 02:30:29 +0330

    finally

--- routes/sensors/dht11/temperature.js
+++ routes/sensors/dht11/temperature.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var resources = require('../../../resources/model');
 
-router.get('/', function (req, resp) {
+router.get('/', function (req, res) {
     res.send(resources.pi.temprature);
 });
 
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var httpServer = require('./server/http');
 var server = httpServer.listen(8000);
 var dhtPlugin = require('./plugins/DHT11');
 
-dhtPlugin.start({'simulate': true, 'frequency': 10000});
+
 console.log("http//127.0.0.1:8000");
+dhtPlugin.start({'hardware': true, 'frequency': 3000});
 
 
  
