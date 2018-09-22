289f4866bdfd25245360a4627e9cc0deb0c8c11e
Pi <h.larian@yahoo.com>
2018-09-22 17:48:18 +0330

    edit

--- routes/sensors/dht11/humidity.js
+++ routes/sensors/dht11/humidity.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var resources = require('../../../resources/model');
 
 router.get('/', function(req, res , next){
-  res.send(resources.pi);
+  res.send(resources.pi.humidity);
+  next();
 });
 
 
--- server/http.js
+++ server/http.js
@@ -3,11 +3,17 @@ var routes = require('../routes/routes');
 var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
-var converter = require('./../middleware/converter');
+var converter = require('../middleWare/converter');
 
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.json()).get(function(req ,res, next){
+next();
+});
+app.use(cors()).get(function(req ,res, next){
+next();
+});
 app.use('/pi' , routes);
-app.use(converter());
+app.use(converter()).get(function(req ,res, next){
+next();
+});
 
 module.exports = app;
