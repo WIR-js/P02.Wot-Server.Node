e771c72d60f146d86ee672f71378856b3d973b2e
Pi <h.larian@yahoo.com>
2018-09-22 17:52:30 +0330

    finally

--- routes/sensors/dht11/temperature.js
+++ routes/sensors/dht11/temperature.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var resources = require('../../../resources/model');
 
 router.get('/', function (req, res, next) {
-    res.send(resources.pi.temprature);
+    res.send(resources.pi.sensors.temprature);
     next();
 });
 
