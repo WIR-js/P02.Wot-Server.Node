794db014b282824b9430b6f3751deb84c26be67d
farzadflp <36715834+farzadflp@users.noreply.github.com>
2018-09-22 17:16:14 +0330

    edit

--- server/http.js
+++ server/http.js
@@ -2,9 +2,12 @@ var express = require('express');
 var routes = require('../routes/routes');
 var app = express();
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var converter = require('./../middleware/converter');
 
+app.use(bodyParser.json());
 app.use(cors());
 app.use('/pi' , routes);
-
+app.use(converter());
 
 module.exports = app;
