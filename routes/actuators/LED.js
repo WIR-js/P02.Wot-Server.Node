4f450a07bf2979b412f3e75dc4118f07c02d72ec
farzadflp <36715834+farzadflp@users.noreply.github.com>
2019-08-29 21:02:28 +0430

    edit

--- routes/actuators/LED.js
+++ routes/actuators/LED.js
@@ -3,11 +3,27 @@ let router = express.Router();
 let resources = require('../../resources/model');
 let ledplugin = require('../../plugins/leds');
 
+/*
+url: ip:port/pi/actuators/leds/
+method: get
+body: ----
+params: ----
+action: send property of leds
+ */
 
 router.route('/').get(function (req, res, next) {
     req.result = resources.pi.actuators.leds;
     next();
 });
+
+/*
+url: ip:port/pi/actuators/leds/:id
+method: get,put
+body: value: true or false
+params: numeric(1 to 4 )
+action: get method : send property of selected led
+        put method : can off or on selected led and send property
+ */
 
 router.route('/:id').get(function (req, res, next) {
     req.result = resources.pi.actuators.leds[req.params.id];
--- routes/sensors/dht11/humidity.js
+++ routes/sensors/dht11/humidity.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var resources = require('../../../resources/model');
+/*
+url: ip:port/pi/sensors/dht11/humidity
+method: get
+body: -----
+params: ----
+action: send humidity property of  dht11 sensor
+ */
 
 router.get('/', function (req, res,next) {
    req.result=resources.pi.sensors.DHT11.humidity;
