2d7353b2a502faeaa880ca990d43c7bbafc036de
farzadflp <36715834+farzadflp@users.noreply.github.com>
2019-08-29 20:12:10 +0430

    edit

--- routes/sensors/dht11/temperature.js
+++ routes/sensors/dht11/temperature.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var resources = require('../../../resources/model');
 
 router.get('/', function (req, res, next) {
-    req.result = resources.pi.sensors.temperature;
+    req.result = resources.pi.sensors.DHT11.temperature;
     //console.info('temperature is: ',resources.pi.sensors.temperature);
     next();
 });
 
 router.put('/alertValue' ,function(req, res, next){
-     resources.pi.sensors.temperature.alertValue = req.body.value;
-     req.result = resources.pi.sensors.temperature;
+     resources.pi.sensors.DHT11.temperature.alertValue = req.body.value;
+     req.result = resources.pi.sensors.DHT11.temperature;
      next();
 });
 
