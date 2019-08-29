76cfec2f2a32f63790266c4a2398a62fdc560982
farzadflp <36715834+farzadflp@users.noreply.github.com>
2019-08-29 20:10:38 +0430

    edit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ console.log("HTTP server is running on http//127.0.0.1:1000");
 
 wsServer.start(server);
 LEDplugin.start({'frequency': 2000, 'LEDs': [LED[1], LED[2],LED[3],LED[4]] });
-dhtPlugin.start({'hardware': true, 'frequency': 5000});
+dhtPlugin.start({'hardware': true, 'frequency': resources.pi.sensors.DHT11.frequency});
 pirPligin.start();
 
 
